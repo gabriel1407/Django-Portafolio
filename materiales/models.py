2f95cd50ee7de33571e7c329b3f1d7385f92b876
Gabriel Carvajal <75456468+gabriel1407@users.noreply.github.com>
2022-04-27 14:02:28 -0400

    gabriel

--- materiales/models.py
+++ materiales/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields import URLField
 
 # Create your models here.
+
+class Project(models.Model):
+    title = CharField(max_length=250)
+    description = CharField(max_length=250)
+    image = models.ImageField (upload_to = 'materiales/images/')
+    url = URLField(blank=True)
