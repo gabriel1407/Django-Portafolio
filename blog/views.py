836b563e96ac1eaff2abedeb11b1fd7e299d2379
Gabriel Carvajal <75456468+gabriel1407@users.noreply.github.com>
2022-04-28 10:41:32 -0400

    gabriel c

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Posts
 
 
 def render_posts(request):
-    return render(request, 'posts.html')
+    posts = Posts.objects.all()
+    return render(request, 'posts.html', {'posts': posts})
