2be045832e158ab454612736a69cd93dac6b7e7b
gabriel <75456468+gabriel1407@users.noreply.github.com>
2022-05-14 16:33:37 -0400

    gabriel

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -82,6 +82,19 @@ DATABASES = {
     }
 }
 
+
+""" 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Portafolio_django',
+        'USER': 'gabriel',
+        'PASSWORD': '28076448',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    }
+}
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
